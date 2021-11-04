9ee27f9d215c3ab9bc3277d1455ce317498e2b28
GitHub Action <action@github.com>
2021-11-04 07:53:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hosttcp/flutterblockgenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hosttcp.flutterblockgenerator.services
+
+import com.github.hosttcp.flutterblockgenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/hosttcp/flutterblockgenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hosttcp/flutterblockgenerator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hosttcp.flutterblockgenerator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hosttcp.flutterblockgenerator.MyBundle
 
 class MyProjectService(project: Project) {
 
